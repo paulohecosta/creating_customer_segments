534ebfbccc05186bbac1ae6a1efb4d1b2166b8bd
paulo costa <paulohecosta@hotmail.com>
2018-01-08 22:00:18 -0200

    Update

--- renders.py
+++ renders.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from sklearn.decomposition import pca
 
 def pca_results(good_data, pca):
